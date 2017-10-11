da1ea09f22e864c6f2c8cb40d88129a278154a76
fen <guangyaol@olymtech.com>
2017-10-11 15:25:40 +0800

    调整多国语言使它兼容最新的 vue-i18n

--- src/locale/index.js
+++ src/locale/index.js
@@ -7,17 +7,18 @@ import Format from './format';
 
 const format = Format(Vue);
 let lang = defaultLang;
-let merged = false;
+// let merged = false;
 let i18nHandler = function() {
     const vuei18n = Object.getPrototypeOf(this || Vue).$t;
     if (typeof vuei18n === 'function') {
-        if (!merged) {
-            merged = true;
-            Vue.locale(
-                Vue.config.lang,
-                deepmerge(lang, Vue.locale(Vue.config.lang) || {}, { clone: true })
-            );
-        }
+        // by FEN 为了兼容最新的 vue-i18n
+        // if (!merged) {
+        //     merged = true;
+        //     Vue.locale(
+        //         Vue.config.lang,
+        //         deepmerge(lang, Vue.locale(Vue.config.lang) || {}, { clone: true })
+        //     );
+        // }
         return vuei18n.apply(this, arguments);
     }
 };
@@ -27,7 +28,8 @@ export const t = function(path, options) {
     if (value !== null && value !== undefined) return value;
 
     const array = path.split('.');
-    let current = lang;
+    // by FEN 为了兼容最新的 vue-i18n , 语言设置有原先的 local 调整为直接设置在 Vue.config.lang 中
+    let current = Vue.config.lang || lang;
 
     for (let i = 0, j = array.length; i < j; i++) {
         const property = array[i];
