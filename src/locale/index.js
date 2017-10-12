2409da9c9046f3cb883503233a60bd2c324e347c
fen <guangyaol@olymtech.com>
2017-10-12 15:03:41 +0800

    调整 i18n 的方法

--- src/locale/index.js
+++ src/locale/index.js
@@ -2,7 +2,7 @@
 
 import defaultLang from './lang/zh-CN';
 import Vue from 'vue';
-import deepmerge from 'deepmerge';
+// import deepmerge from 'deepmerge';
 import Format from './format';
 
 const format = Format(Vue);
@@ -11,7 +11,7 @@ let lang = defaultLang;
 let i18nHandler = function() {
     const vuei18n = Object.getPrototypeOf(this || Vue).$t;
     if (typeof vuei18n === 'function') {
-        // by FEN 为了兼容最新的 vue-i18n
+      // by FEN 为了兼容最新的 vue-i18n
         // if (!merged) {
         //     merged = true;
         //     Vue.locale(
@@ -28,8 +28,7 @@ export const t = function(path, options) {
     if (value !== null && value !== undefined) return value;
 
     const array = path.split('.');
-    // by FEN 为了兼容最新的 vue-i18n , 语言设置有原先的 local 调整为直接设置在 Vue.config.lang 中
-    let current = Vue.config.lang || lang;
+    let current = lang;
 
     for (let i = 0, j = array.length; i < j; i++) {
         const property = array[i];
