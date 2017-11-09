7fc3aa2e6c4a175ac3b9039c3bd147021dc1bd9f
fen <fen@fenth.com>
2017-11-09 23:15:10 +0800

    datepick 双向绑定 array 模式下修复

--- examples/routers/date.vue
+++ examples/routers/date.vue
@@ -10,79 +10,80 @@
         <Button @click="getDate">get date</Button>
 
         <Form ref="formValidate" :model="formValidate" :rules="ruleValidate" :label-width="80">
-        <FormItem label="1" prop="date">
-            <Date-picker v-model="formValidate.date" type="date" placeholder="选择日期" style="width: 200px"></Date-picker>
-        </FormItem>
-        <FormItem label="232" prop="num">
-            <Input v-model="formValidate.num" number style="width: 200px"></Input>
-        </FormItem>
-        <Button @click="vdate">vdate</Button>
+            <FormItem label="1" prop="date">
+                <Date-picker v-model="formValidate.date" type="date" placeholder="选择日期" style="width: 200px"></Date-picker>
+            </FormItem>
+            <FormItem label="232" prop="num">
+                <Input v-model="formValidate.num" number style="width: 200px"></Input>
+            </FormItem>
+            <Button @click="vdate">vdate</Button>
         </Form>
         <!--<Date-picker v-model="value2" type="daterange" placeholder="选择日期" style="width: 200px"></Date-picker>-->
         <!--<Date-picker transfer type="datetimerange" placeholder="选择日期" style="width: 200px" @on-change="changeDate"></Date-picker>-->
     </div>
 </template>
 <script>
-    export default {
-        data () {
-            return {
-                value1: ['2016.12.12', '2017-10-10'],
-                value2: '2017-12-12',
-                value3: 1508143679588,
-                value5: '',
-                value4: '2017-12-12',
-                formValidate: {
-                    date: 1508143679588,
-                    num: 0
-                },
-                ruleValidate: {
-                    num: { required: true, type: 'number', message: 'Please select the date', trigger: 'change' },
-                    date: { required: true, type: 'number', length:13, message: 'Please select the date', trigger: 'change' },
-                },
-            }
+export default {
+    data () {
+        return {
+            value11: [1508143679588, 1513008000000],
+            value1: ['2017-12-12', '2017-12-12'],
+            value2: '2017-12-12',
+            value3: 1508143679588,
+            value5: '',
+            value4: '2017-12-12',
+            formValidate: {
+                date: 1508143679588,
+                num: 0
+            },
+            ruleValidate: {
+                num: { required: true, type: 'number', message: 'Please select the date', trigger: 'change' },
+                date: { required: true, type: 'number', length: 13, message: 'Please select the date', trigger: 'change' },
+            },
+        }
+    },
+
+    created () {
+        const date = new Date()
+        date.setTime(this.value2)
+        console.log(date.getTime(), 22)
+        console.log(this.value2.toString().length)
+    },
+    methods: {
+        vdate () {
+            this.$refs.formValidate.validate((valid) => {
+                if (valid) {
+                    this.$Message.success('Success!');
+                } else {
+                    this.$Message.error('Fail!');
+                }
+            })
         },
-    
-        created (){
+        changeDate (date) {
+            console.log(date);
+        },
+        setDate () {
+            this.value1 = ['2014-10-10', '2017-10-10'];
+            this.value2 = '1508143679588';
+        },
+        getDate () {
             const date = new Date()
-            date.setTime(this.value2)
-            console.log(date.getTime(), 22)
-            console.log(this.value2.toString().length)
+            console.log(this.value1, 'value1')
+            console.log(this.value2, 'value2')
+            console.log(this.value3, 'value3')
+            console.log(this.value4, 'value4')
+            console.log(this.formValidate.date, 'date')
         },
-        methods: {
-            vdate () {
-                this.$refs.formValidate.validate((valid) => {
-                    if (valid) {
-                        this.$Message.success('Success!');
-                    } else {
-                        this.$Message.error('Fail!');
-                    }
-                })
-            },
-            changeDate(date){
-                console.log(date);
-            },
-            setDate () {
-                this.value1 = ['2014-10-10', '2017-10-10'];
-                this.value2 = '1508143679588';
-            },
-            getDate () {
-                const date = new Date()
-                console.log(this.value1, 'value1')
-                console.log(this.value2, 'value2')
-                console.log(this.value3, 'value3')
-                console.log(this.value4, 'value4')
-                console.log(this.formValidate.date, 'date')
-            },
-            hc (d) {
-                console.log(d);
-            }
+        hc (d) {
+            console.log(d);
         }
     }
+}
 </script>
 <style>
-    body{
-        width: 100%;
-    }
+body {
+    width: 100%;
+}
 </style>
 
 
