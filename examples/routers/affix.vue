8f7493c0eb62b859965a104c7d7fd6621a61d871
fen <guangyaol@olymtech.com>
2017-11-27 18:09:45 +0800

    删除用不到的图标，增加排序图标

--- examples/routers/affix.vue
+++ examples/routers/affix.vue
@@ -10,9 +10,15 @@
     }
 </style>
 <template>
-    <Affix>
-        <span class="demo-affix">固定在最顶部</span>
-    </Affix>
+    <div>
+        <Icon type="checkmark-circled"></Icon>
+        <Icon type="close-circled"></Icon>
+        <Icon type="arrow-return-left"></Icon>
+        <Icon type="sort-alpha-ase"></Icon>
+        <Affix>
+            <span class="demo-affix">固定在最顶部</span>
+        </Affix>
+    </div>
 </template>
 <script>
     export default {
